0245b64ce9e7fbd8e8a74364c0165b6f4bdb04ac
hagevvashi <hagevvashi@gmail.com>
2021-04-21 20:30:40 +0900

    test

--- test.sh
+++ test.sh
@@ -2,3 +2,7 @@
 set -euo pipefail
 
 echo '/deploy ho1ge/f-o_o' | grep -E '^\/deploy\s+((\w|\d|\_|\-|\/)+)$'
+
+if echo '/deploy ho1ge/f-o_o' | grep -q -E '^\/deploy\s+((\w|\d|\_|\-|\/)+)$'; then
+  echo 'foo'
+fi
